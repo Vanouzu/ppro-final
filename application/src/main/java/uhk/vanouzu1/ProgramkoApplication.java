9140bcddb6dda81e1c72bf2729319dcaca04adb7
Zuzana Vanousova <anazuzuzana@centrum.cz>
2018-02-01 19:04:20 +0100

    Initial commit

---
+++ application/src/main/java/uhk/vanouzu1/ProgramkoApplication.java
@@ -0,0 +1,23 @@
+package uhk.vanouzu1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * = ProgramkoApplication
+ *
+ * TODO Auto-generated class documentation
+ *
+ */
+@SpringBootApplication
+public class ProgramkoApplication {
+
+    /**
+     * TODO Auto-generated method documentation
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(ProgramkoApplication.class, args);
+    }
+}
